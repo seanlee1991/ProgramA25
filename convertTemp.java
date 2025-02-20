b6bef6dca625e978d47300f6fa582a41a38973f5
seanlee1991 <164179888+seanlee1991@users.noreply.github.com>
2025-02-20 14:09:33 +1100

    Add files via upload

---
+++ convertTemp.java
@@ -0,0 +1,16 @@
+public class convertTemp {
+    public static void main(String[] args) {
+        
+        Double fahrenheit = 98.0;
+        Double celsius;
+
+        celsius = (5.0/9.0) * (fahrenheit - 32.0);
+        
+
+        
+        
+
+        System.out.println(celsius);
+        
+    }
+}
